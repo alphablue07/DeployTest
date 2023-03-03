ac0b434b459ff6e46f0007dd5fa212b28aa0baf4
Audi Ramli <audiutaryo@gmail.com>
2023-03-03 22:02:29 +0700

    deploy tester

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,21 +9,13 @@ import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-  apiKey: "AIzaSyAG59xDz-dhD402QS18ovBFTs8MIm5t99s",
-  authDomain: "st-project-f2c80.firebaseapp.com",
-  databaseURL: "https://st-project-f2c80-default-rtdb.firebaseio.com",
-  projectId: "st-project-f2c80",
-  storageBucket: "st-project-f2c80.appspot.com",
-  messagingSenderId: "105374234602",
-  appId: "1:105374234602:web:73556ef5ff52146623e493",
-  measurementId: "G-GPRFGQPD3H"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
 };
 
 // Initialize Firebase
